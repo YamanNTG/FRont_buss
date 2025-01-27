75456b3e099e37287ad3f04a04cfd38034e967b2
YamanNTG <claudiuoprea21@gmail.com>
2025-01-27 22:18:43 +0000

    change to url

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const customFetch = axios.create({
-  baseURL: 'https://dbuss-api-025-8594a98bd0c9.herokuapp.com',
+  baseURL: '/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
