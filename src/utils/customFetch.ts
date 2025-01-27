3dd1d3dc07e410dbd9a1225d34d8bfc5f6ddf4c1
YamanNTG <claudiuoprea21@gmail.com>
2025-01-27 22:20:52 +0000

    url update

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const customFetch = axios.create({
-  baseURL: '/api/v1',
+  baseURL: '',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
