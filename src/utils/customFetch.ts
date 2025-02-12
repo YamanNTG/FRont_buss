f2e0babfb011c74979e83302c86e5891d904aa59
YamanNTG <claudiuoprea21@gmail.com>
2025-02-12 16:30:23 +0000

    Upload Image fixed

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -9,9 +9,10 @@ export const customFetch = axios.create({
 });
 
 export const customImageFetch = axios.create({
-  baseURL: 'https://dbuss-api-025-8594a98bd0c9.herokuapp.com/',
+  baseURL: 'https://dbuss-api-025-8594a98bd0c9.herokuapp.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'multipart/form-data',
+    'Access-Control-Allow-Credentials': 'true',
   },
 });
