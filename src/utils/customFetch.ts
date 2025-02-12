5094df252b3652a8843814d0195a6135622aec81
YamanNTG <claudiuoprea21@gmail.com>
2025-02-12 16:44:26 +0000

    Update on fetch with empty strings

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -9,7 +9,7 @@ export const customFetch = axios.create({
 });
 
 export const customImageFetch = axios.create({
-  baseURL: 'https://dbuss-api-025-8594a98bd0c9.herokuapp.com',
+  baseURL: '',
   withCredentials: true,
   headers: {
     'Content-Type': 'multipart/form-data',
